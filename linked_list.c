b86aa14717cebd223d51f6f877e3d9aed7fd3b92
Matei-Gatin <thedackchair@gmail.com>
2026-07-16 14:16:29 +0200

    Implemented linked list operations: reverse_list, find_middle, search, print_linked_list_recursive, print_linked_list_recursive_reversed

--- linked_list.c
+++ linked_list.c
@@ -20,6 +20,10 @@ Node* delete_first(Node *HEAD);
 Node* delete_last(Node *HEAD);
 Node* delete(Node *HEAD, int pos);
 
+Node* reverse_list(Node* HEAD);
+
+int search(Node* HEAD, int target);
+
 void free_list(Node *HEAD);
 
 //
@@ -146,7 +150,6 @@ Node* delete(Node *HEAD, const int pos) {
         return NULL;
     }
 
-    // 1 -> 2 -> 3
     if (pos == 0) {
         return delete_first(HEAD);
     }
@@ -160,11 +163,82 @@ Node* delete(Node *HEAD, const int pos) {
         walker = walker->next;
     }
 
+    Node *node_to_del = walker->next;
+    walker->next = walker->next->next;
 
+    free(node_to_del);
 
     size--;
 
     return HEAD;
+}
+
+Node* reverse_list(Node* HEAD) {
+    if (HEAD == NULL) {
+        printf("The list is empty.\n");
+        return NULL;
+    }
+
+    Node *prev = NULL;
+    Node *current = HEAD;
+    Node *next = NULL;
+
+    while (current != NULL) {
+        // STEP 1 -> store next
+        next = current->next;
+
+        // STEP 2 -> point current->next to prev
+        current->next = prev;
+
+        // STEP 3 -> move prev to current
+        prev = current;
+
+        // STEP 4 -> move current to next
+        current = next;
+    }
+
+    return prev;
+}
+
+int search(Node* HEAD, const int target) {
+    // searches for target and returns the index if target exist
+    // else return -1
+
+    if (HEAD == NULL) {
+        printf("The list is empty.\n");
+        return -1;
+    }
+
+    int index = 0;
+
+    Node *walker = HEAD;
+    while (walker != NULL) {
+        if (walker->data == target) {
+            return index;
+        }
+
+        index++;
+        walker = walker->next;
+    }
+
+    return -1;
+}
+
+int find_middle(Node* HEAD) {
+    if (HEAD == NULL) {
+        printf("List is empty\n");
+        return -1;
+    }
+
+    Node *slow = HEAD;
+    Node *fast = HEAD;
+
+    while (fast != NULL && fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return slow->data;
 }
 
 void print_linked_list(const Node *HEAD) {
@@ -177,6 +251,17 @@ void print_linked_list(const Node *HEAD) {
     }
 
     printf("NULL\n");
+}
+
+void print_linked_list_recursive(const Node *HEAD) {
+    // base case
+    if (HEAD == NULL) {
+        printf("NULL\n");
+        return;
+    }
+
+    printf("%d -> ", HEAD->data);
+    print_linked_list_recursive(HEAD->next);
 }
 
 void free_list(Node *HEAD) {
@@ -196,20 +281,31 @@ void free_list(Node *HEAD) {
 int main(void) {
     Node *head = NULL;
 
+    // head = add_first(head, 5);
+    head = add_first(head, 4);
     head = add_first(head, 3);
     head = add_first(head, 2);
     head = add_first(head, 1);
-    //
+
     // head = add(head, 100, 1);
     // head = add(head, 200, 0);
     // head = add(head, 300, 5);
     // head = add_last(head, 400);
 
     // head = delete_first(head);
+    // head = delete_last(head);
+    // head = delete(head, 1);
 
-    head = delete_last(head);
-    print_linked_list(head);
-    printf("size: %d", size);
+    head = reverse_list(head);
+
+    // int value_to_search = 3;
+    // int target = search(head, value_to_search);
+
+    // int middle = find_middle(head);
+
+    print_linked_list_recursive(head);
+    // printf("Value %d -> Index: %d", value_to_search, target);
+    // printf("Middle value: %d\n", middle);
 
     free_list(head);
 
