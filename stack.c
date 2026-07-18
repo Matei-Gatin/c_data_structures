e47e5b0b253be7ed53821a71a0564d691b946b8f
Matei-Gatin <thedackchair@gmail.com>
2026-07-18 16:23:49 +0200

    Stack: push(), pop(), resize(), print()

--- stack.c
+++ stack.c
@@ -1,3 +1,79 @@
 //
 // Created by matt on 7/18/26.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// GLOBAL VARIABLES
+int *stack;
+int top = -1;
+int max_size = 10;
+
+// PROTOTYPES
+int pop();
+void resize();
+void push(int value);
+void print();
+
+// FUNCTIONS
+void push(const int value) {
+    if (top + 1 == max_size) {
+        resize();
+    }
+
+    top++;
+    stack[top] = value;
+}
+
+int pop() {
+    if (top == -1) {
+        printf("Stack Underflow! The stack is empty.\n");
+        exit(1);
+    }
+
+    const int value = stack[top];
+    top--;
+    return value;
+}
+
+void resize() {
+    max_size *= 2;
+    int *temp = realloc(stack, max_size * sizeof(int));
+
+    if (temp == NULL) {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+
+    stack = temp;
+}
+
+void print() {
+    if (top == -1) {
+        printf("Stack is empty.\n");
+        return;
+    }
+
+    for (int i = 0; i <= top; i++) {
+        if (i == top) {
+            printf("%d\n", stack[i]);
+            return;
+        }
+
+        printf("%d, ", stack[i]);
+    }
+}
+
+int main(void) {
+    stack = malloc(max_size * sizeof(int));
+
+    if (stack == NULL) return 1;
+
+    for (int i = 100; i <= 1200; i += 100) {
+        push(i);
+    }
+    print();
+
+    return 0;
+}
